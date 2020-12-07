ec336b723fb59d96fca184cd46825d1f0c36d391
nixon501 <71940084+nixon501@users.noreply.github.com>
2020-12-07 13:45:57 -0500

    Add about us page, and changed footers.

--- PropertyPlusApp/Controllers/HomeController.cs
+++ PropertyPlusApp/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace PropertyPlusApp.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult AboutUs()
         {
             return View();
         }
